7148f88b35328b0441a179a71c7ec4dcb47ba306
Nilesh Pawar <pawar.nilesh64@gmail.com>
2022-11-03 19:42:31 +0530

    Javascript

--- Javascript/myutils.js
+++ Javascript/myutils.js
@@ -1,4 +1,29 @@
+var Name = "Anand";
 function add() {
-    var c  = 2+4;
+    var Name = 'Nilesh';
+    var c  = 2+4 // it is returning addition
+    var C = 5+10
     console.log('Addition: ', c);
+    /*
+     werwrewr
+     wrwer
+     wrewre
+    */
 }
+/*
+    This is a function returning addition
+    @param a
+    @param b
+*/
+function ADD(a, b) {
+    var myString = "This is my string";
+    var myVar = 20;
+    var myVar2 = 30.40;
+    return 3+5;
+}
+var a = "String";
+a = 40;
+var obj = {
+ name: 'Nilesh',
+ phone: 9987656544,
+};
